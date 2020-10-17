45e016c3fb8b3f0ef5bc4b2c2342204c07afd371
Ian Eve Perry <ian.eve.perry@gmail.com>
2020-10-16 23:44:21 -0700

    setup infinite looping and writing to db

--- get_recent_tracks.R
+++ get_recent_tracks.R
@@ -1,13 +1,31 @@
 library(tidyverse)
 library(spotifyr)
+library(RSQLite)
 
 source("api_keys.R")
 
 spotify_auth_code = get_spotify_authorization_code(client_id = spotify_client_id, client_secret = spotify_client_secret, scope = "user-read-recently-played")
 
-get_recent_tracks = function(limit = 50, authorization, ...) {
-  get_my_recently_played(limit = limit,  authorization = authorization, ...) %>%
-    mutate(timestamp = lubridate::ymd_hms(played_at))
+db = dbConnect(RSQLite::SQLite(), "./scrobbles.sqlite")
+
+query = paste("SELECT MAX(timestamp) FROM api_tracks_played")
+
+latest_timestamp = unlist(dbGetQuery(db, query))
+
+get_recent_tracks = function(authorization = spotify_auth_code) {
+  response = get_my_recently_played(limit = 50, authorization = authorization, after = latest_timestamp)
+  if (is.data.frame(response)) {
+    recent_tracks = response %>% 
+      select(timestamp = played_at, spotify_id = track.id, track_name = track.name)
+    
+    dbAppendTable(db, "api_tracks_played", recent_tracks)
+    
+    latest_timestamp <<- max(recent_tracks$timestamp)
+  }
+  
+  later::later(get_recent_tracks, 60)
+  
+  print(latest_timestamp)
 }
 
-recent_tracks = get_recent_tracks(authorization = spotify_auth_code)
+get_recent_tracks()
