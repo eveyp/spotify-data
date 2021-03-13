eec0bee6bca0400b5c9128a6ef43a86109e9d0ce
Eve Perry <ian.eve.perry@gmail.com>
2021-03-12 23:22:24 -0800

    finish up

--- populate_db.py
+++ populate_db.py
@@ -1,32 +1,12 @@
-from sqlalchemy import and_, create_engine
-from sqlalchemy.orm import sessionmaker
+import scrobbler
 from models import Album, Artist, Scrobble, Track
-from helpers import get_db_path, get_spotify_api_keys, get_spotify_api
-from database_stuff import add_track, add_artist, add_album
-
-key_file = "api_keys.yaml"
-
-client_secret, client_id = get_spotify_api_keys(key_file)
-
-sp = get_spotify_api(client_secret, client_id)
-
-db_path = get_db_path(key_file)
-
-engine = create_engine(f"sqlite:///{db_path}")
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
 
 
 def simplify_db_result(db_result):
     # the tracks come back as a list of sqlalchemy result objects which are basically tuples of length 1
     # the spotify api package can't handle this so we need to parse into a tuple of strings
-    # first we unpack the list of result objects using *track
-    # then we use zip to grab the first element in each result object
-    # then we convert the resulting zip object into a list that contains one tuple with all the track id's
-    # then we just grab that tuple, ie. the first element in the list
-    return tuple(zip(*db_result))[0]
-
+    # we use a list comprehension to grab the first element from each tuple and put it in a list
+    return [i[0] for i in db_result]
 
 def chunk(long_list, chunk_size=50):
     # break the tuple of tracks into a list of tuples of length 50 b/c that's the limit for one api request
@@ -44,66 +24,122 @@ def dechunk(chunks):
         return [item for chunk in chunks for item in chunk[key]]
 
 
-def populate_db(session, sp):
-    # get the unique list of tracks that are scrobbled but not already in the tracks table
-    tracks = simplify_db_result(
-        session.query(Scrobble.spotify_id).distinct().filter(~Scrobble.spotify_id.in_(
-            session.query(Track.spotify_id).distinct())
-        ).all()
-    )
-    
-    
+class Populator(scrobbler.Scrobbler):
+    def get_new_tracks(self):
+        # get the unique list of tracks that are scrobbled but not already in the tracks table
+        tracks = simplify_db_result(
+            self.db.query(Scrobble.spotify_id).distinct().filter(~Scrobble.spotify_id.in_(
+                self.db.query(Track.spotify_id).distinct())
+            ).all()
+        )
+
+        # don't want any None's so remove them if they are in there
+        if None in tracks:
+            tracks.remove(None)
+
+        return tracks
+
+    def get_new_artists(self, tracks):
+        new_track_artists = {track['artists'][0]['id'] for track in tracks}
+
+        existing_artist_ids = simplify_db_result(
+            self.db.query(Artist.spotify_id).distinct()
+        )
+
+        new_artist_ids = [
+            id for id in new_track_artists if id not in existing_artist_ids]
+
+        # don't want any None's so remove them if they are in there
+        if None in new_artist_ids:
+            new_artist_ids.remove(None)
+
+        return new_artist_ids
+
+
+    def get_new_albums(self, tracks):
+        new_track_albums = {track['album']['id'] for track in tracks}
+
+        existing_album_ids = simplify_db_result(
+            self.db.query(Album.spotify_id).distinct()
+        )
+
+        new_album_ids = [
+            id for id in new_track_albums if id not in existing_album_ids]
+
+        # don't want any None's so remove them if they are in there
+        if None in new_album_ids:
+            new_album_ids.remove(None)
+
+        return new_album_ids
+
+
+def main(populator: Populator):
+    new_tracks = populator.get_new_tracks()
+
+    if len(new_tracks) == 0:
+        print("No new tracks, exiting.")
+        return
+
+    print(f'Found {len(new_tracks)} new tracks.')
+
+    print("Fetching track data.")
     track_data = dechunk(
         # iterate over that list of chunks, hitting the spotify api for the track data in each chunk
-        [sp.tracks(chunk) for chunk in chunk(tracks)]
+        [populator.sp.tracks(chunk) for chunk in chunk(new_tracks)]
     )
 
+    print("Fetching tracks feature data.")
     track_feature_data = dechunk(
         # iterate over that list of chunks, hitting the spotify api for the track feature data in each chunk
-        [sp.audio_features(chunk) for chunk in chunk(tracks, 100)]
+        [populator.sp.audio_features(chunk) for chunk in chunk(new_tracks, 100)]
     )
 
-    for track_data, track_features in zip(track_data, track_feature_data):
-        add_track(session, sp, track_data=track_data, track_features=track_features)
-    
-    session.commit()
+    print("Inserting new tracks into database.")
+    for data, features in zip(track_data, track_feature_data):
+        populator.add_track(track_data=data, track_features=features)
 
-    artist_ids = { track['artists'][0]['id'] for track in track_data }
+    populator.db.commit()
 
-    existing_artist_ids = simplify_db_result(
-        session.query(Artist.spotify_id).distinct()
-    )
+    new_artist_ids = populator.get_new_artists(track_data)
 
-    new_artist_ids = [ id for id in artist_ids if id not in existing_artist_ids ]
+    print(f'Found {len(new_artist_ids)} new artists.')
 
-    artist_data = dechunk(
-        [ sp.artists(artist_chunk) for artist_chunk in chunk(new_artist_ids) ]
-    )
-    
-    for artist in artist_data:
-        add_artist(session, sp, artist_data=artist)
+    if len(new_artist_ids) > 0:
+        print("Fetching artist data.")
+        artist_data = dechunk(
+            [populator.sp.artists(artist_chunk)
+             for artist_chunk in chunk(new_artist_ids)]
+        )
 
-    session.commit()
+        print("Inserting new artists into database.")
+        for artist in artist_data:
+            populator.add_artist(artist_data=artist)
 
-    album_ids = {track['album']['id'] for track in track_data}
+        populator.db.commit()
 
-    existing_album_ids = simplify_db_result(
-        session.query(Album.spotify_id).distinct()
-    )
+    new_album_ids = populator.get_new_albums(track_data)
 
-    new_album_ids = [id for id in album_ids if id not in existing_album_ids]
+    print(f'Found {len(new_album_ids)} new albums.')
 
-    album_data = dechunk(
-        [sp.albums(album_chunk) for album_chunk in chunk(new_album_ids, 20)]
-    )
+    if len(new_album_ids) > 0:
+        print("Fetching album data.")
+        album_data = dechunk(
+            [populator.sp.albums(album_chunk)
+             for album_chunk in chunk(new_album_ids, 20)]
+        )
 
-    for album in album_data:
-        add_album(session, sp, album_id=None, album_data=album)
-    
-    session.commit()
+        print("Inserting new albums into database.")
+        for album in album_data:
+            populator.add_album(album_data=album)
+
+        populator.db.commit()
+
+    print("All done!")
 
     return
 
 
 if __name__ == '__main__':
-    populate_db()
+    populator = Populator('settings.yaml')
+
+    main(populator)
